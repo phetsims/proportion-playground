84634be7b3ccfab62cffa4ac6f67caf22ea6f31e
samreid <reids@colorado.edu>
2016-05-22 20:25:42 -0600

    Added grid lines

--- js/explore/view/billiards/BilliardsTableNode.js
+++ js/explore/view/billiards/BilliardsTableNode.js
@@ -12,8 +12,11 @@ define( function( require ) {
   var proportionPlayground = require( 'PROPORTION_PLAYGROUND/proportionPlayground' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Line = require( 'SCENERY/nodes/Line' );
 
   function BilliardsTableNode( center, billiardsTableModel ) {
+
+    // Model the edge outside of the green area (not as a stroke) since there is no way to do "outer" stroke
     var brownRectangle = new Rectangle( 0, 0, 0, 0, { fill: '#73481d' } );
     var greenRectangle = new Rectangle( 0, 0, 0, 0, { fill: '#0a6739' } );
 
@@ -21,9 +24,32 @@ define( function( require ) {
       var length = billiardsTableModel.length;
       var width = billiardsTableModel.width;
 
-      var scale = 12;
-      greenRectangle.setRect( 0, 0, width * scale, length * scale );
+      var scale = 20;
+      var brownEdgeLineWidth = 8;
+      var scaledWidth = width * scale;
+      var scaledHeight = length * scale;
+      var lineWidthAmount = brownEdgeLineWidth * 2;
+      brownRectangle.setRect( 0, 0, scaledWidth + lineWidthAmount, scaledHeight + lineWidthAmount );
+      greenRectangle.setRect( 0, 0, scaledWidth, scaledHeight );
+      brownRectangle.center = center;
       greenRectangle.center = center;
+
+      var createGridLines = function() {
+        var gridLines = [];
+
+        // vertical lines
+        for ( var i = 0; i <= width; i++ ) {
+          gridLines.push( new Line( i * scale, 0, i * scale, scaledHeight, { stroke: 'white' } ) );
+        }
+
+        // horizontal lines
+        for ( var k = 0; k <= length; k++ ) {
+          gridLines.push( new Line( 0, k * scale, scaledWidth, k * scale, { stroke: 'white' } ) );
+        }
+        return gridLines;
+      };
+      // grid lines
+      greenRectangle.children = createGridLines();
     };
 
     billiardsTableModel.lengthProperty.link( updateTable );
