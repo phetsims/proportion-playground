70f93a056dd4a9b907c21a50b10b93e58effff34
samreid <reids@colorado.edu>
2016-05-24 18:26:49 -0600

    Colorized paint spinners

--- js/explore/view/paint/ControllableSplotchNode.js
+++ js/explore/view/paint/ControllableSplotchNode.js
@@ -16,16 +16,23 @@ define( function( require ) {
   var Property = require( 'AXON/Property' );
   var HBox = require( 'SCENERY/nodes/HBox' );
   var SplotchNode = require( 'PROPORTION_PLAYGROUND/explore/view/paint/SplotchNode' );
+  var ColorMap = require( 'PROPORTION_PLAYGROUND/explore/view/paint/ColorMap' );
 
   function ControllableSplotchNode( splotchModel, grayscaleProperty, revealProperty ) {
     var numberPickerOptions = { scale: 2 };
-    var roundBeadNumberPicker = new NumberPicker( splotchModel.color1CountProperty, new Property( splotchModel.colorCountRange ), numberPickerOptions );
-    var squareBeadNumberPicker = new NumberPicker( splotchModel.color2CountProperty, new Property( splotchModel.colorCountRange ), numberPickerOptions );
+    var roundBeadNumberPicker = new NumberPicker( splotchModel.color1CountProperty, new Property( splotchModel.colorCountRange ), _.extend( {
+      color: ColorMap.getColor( 0 )
+    }, numberPickerOptions ) );
+    var squareBeadNumberPicker = new NumberPicker( splotchModel.color2CountProperty, new Property( splotchModel.colorCountRange ), _.extend( {
+      color: ColorMap.getColor( 1 )
+    }, numberPickerOptions ) );
 
+    // TODO: Black/white spinners for black/white mode
+    
     // Wrap in a node so the visible flags don't collide
     var splotchNode = new Node( { children: [ new SplotchNode( splotchModel.color1CountProperty, splotchModel.color2CountProperty, grayscaleProperty ) ] } );
     revealProperty.linkAttribute( splotchNode, 'visible' );
-    
+
     Node.call( this, {
       children: [
         splotchNode,
