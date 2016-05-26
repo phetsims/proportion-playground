1dd404af532b9838ee1552a31bb6956f8c384204
samreid <reids@colorado.edu>
2016-05-25 20:19:35 -0600

    Removed completed TODOs

--- js/explore/model/paint/SplotchModel.js
+++ js/explore/model/paint/SplotchModel.js
@@ -26,7 +26,7 @@ define( function( require ) {
       color2Count: 0
     } );
 
-    this.colorCountRange = new Range( 0, 20 ); // TODO: Factor out ranges
+    this.colorCountRange = new Range( 0, 20 );
 
     // These assignments provide improved highlighting and navigation in IntelliJ IDEA 
     this.color1CountProperty = this.color1CountProperty || null;
@@ -42,11 +42,13 @@ define( function( require ) {
       // compare ratios between sims, accounting for /0
       return this.ratio1 === appleGroupModel.ratio1 && this.ratio2 === appleGroupModel.ratio2;
     },
+
+    // @private
     get ratio1() {
       return this.color1Count / this.color2Count;
     },
 
-    // TODO: this hack makes it easy to compare ratios between sims, accounting for /0
+    // @private
     get ratio2() {
       return this.color2Count / this.color1Count;
     }
