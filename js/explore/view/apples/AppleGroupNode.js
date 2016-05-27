34b81daa9b428bbfdcf8960c8bf3a04e394f45bf
samreid <reids@colorado.edu>
2016-05-27 13:24:51 -0600

    Added docs

--- js/explore/view/apples/AppleGroupNode.js
+++ js/explore/view/apples/AppleGroupNode.js
@@ -1,6 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
+ * Shows a group of apples which are all the same color (green or red)
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -21,29 +22,33 @@ define( function( require ) {
   var coinImage = require( 'mipmap!PROPORTION_PLAYGROUND/coin.png' );
 
   // constants
-  var appleScale = 0.35;
-  var coinScale = 0.7;
+  var appleImageScale = 0.35; // Reduction factor for showing the image
+  var coinImageScale = 0.7; // Reduction factor for showing the image
   var applesPerRow = 5;
+  var distanceBetweenCoinsAndApples = 65;
 
   function AppleGroupNode( appleGroupModel, appleImage, showCostPerAppleProperty ) {
     var yOffsetOptions = { y: 320 };
     var appleLayer = new Node( yOffsetOptions );
-    var coinLayer = new Node( { y: 320 } );
+    var coinLayer = new Node( yOffsetOptions );
     var priceTagLayer = new Node( { x: -140, y: 120 } );
-    var appleLayerX = -applesPerRow * appleImage[ 0 ].width * appleScale / 2;
-    var distanceBetweenCoinsAndApples = 65;
+    var appleLayerX = -applesPerRow * appleImage[ 0 ].width * appleImageScale / 2;
     Node.call( this, {
       children: [ coinLayer, appleLayer, priceTagLayer ]
     } );
+
+    // Show/hide the price tag
     showCostPerAppleProperty.link( function( showCostPerApple ) {
       priceTagLayer.visible = showCostPerApple;
     } );
+
+    // Show the grid of apples
     appleGroupModel.numberOfApplesProperty.link( function( numberOfApples ) {
       var appleArray = [];
       var x = 0;
       var y = 0;
       for ( var i = 0; i < numberOfApples; i++ ) {
-        var image = new Image( appleImage, { scale: appleScale, x: x, y: y } );
+        var image = new Image( appleImage, { scale: appleImageScale, x: x, y: y } );
         appleArray.unshift( image ); // prepend to get z-order correct
         x = image.right;
         if ( x >= image.width * applesPerRow ) {
@@ -56,19 +61,21 @@ define( function( require ) {
       appleLayer.x = appleLayerX;
     } );
 
+    // Show the stack of coins
     appleGroupModel.totalCostProperty.link( function( totalCost ) {
       var coinArray = [];
       var x = 0;
       var y = 0;
       for ( var i = 0; i < totalCost; i++ ) {
-        var image = new Image( coinImage, { scale: coinScale, x: x, y: y } );
+        var image = new Image( coinImage, { scale: coinImageScale, x: x, y: y } );
         coinArray.push( image );
         y = y - image.height / 6;
       }
       coinLayer.children = coinArray;
-      coinLayer.x = appleLayerX - coinImage[ 0 ].width * coinScale - distanceBetweenCoinsAndApples;
+      coinLayer.x = appleLayerX - coinImage[ 0 ].width * coinImageScale - distanceBetweenCoinsAndApples;
     } );
 
+    // Update the price tag
     Property.multilink( [ appleGroupModel.totalCostProperty, appleGroupModel.numberOfApplesProperty ], function( totalCost, numberOfApples ) {
       var pricePerApple = totalCost / numberOfApples;
       var fixed = Util.toFixed( pricePerApple, 2 );
