fd41082b3a81b908f2d837482af82e293786cc32
samreid <reids@colorado.edu>
2016-05-27 13:38:13 -0600

    Added docs

--- js/explore/view/billiards/BilliardsSceneNode.js
+++ js/explore/view/billiards/BilliardsSceneNode.js
@@ -1,6 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
+ * The node for the Billiards Scene, including two tables and spinners for each.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -16,12 +17,23 @@ define( function( require ) {
   var HBox = require( 'SCENERY/nodes/HBox' );
   var ExploreSceneNode = require( 'PROPORTION_PLAYGROUND/explore/view/ExploreSceneNode' );
 
+  /**
+   *
+   * @param {Bounds2} layoutBounds - the region within which all visual components should be layed out
+   * @param {BilliardsSceneNode} billiardsSceneModel - the model
+   * @param {boolean} predictMode - true for the Predict Screen which has a reveal button
+   * @constructor
+   */
   function BilliardsSceneNode( layoutBounds, billiardsSceneModel, predictMode ) {
     var billiardsSceneNode = this;
+
+    // Create the left/right tables
     var billiardsTableNode1 = new BilliardsTableNodeWithSpinners( layoutBounds, billiardsSceneModel.table1, billiardsSceneModel.revealProperty );
     var billiardsTableNode2 = new BilliardsTableNodeWithSpinners( layoutBounds, billiardsSceneModel.table2, billiardsSceneModel.revealProperty, {
       side: 'right'
     } );
+
+    // Create the switch that toggles between 1 or 2 tables showing.
     var scaleOptions = { scale: 0.3 };
     var abSwitch = new ABSwitch( billiardsSceneModel.showBothProperty,
       false, new BilliardTableIcon( 120, 120, scaleOptions ),
@@ -36,8 +48,8 @@ define( function( require ) {
     ExploreSceneNode.call( this, layoutBounds, billiardsSceneModel, predictMode, 60, {
       children: [ billiardsTableNode1, billiardsTableNode2, abSwitch ]
     } );
-    this.necklaceSceneModel = billiardsSceneModel;
 
+    // When the ABSwitch is toggled, show/hide the rightmost table and update the layout.
     billiardsSceneModel.showBothProperty.link( function( showBoth ) {
       billiardsTableNode2.visible = showBoth;
 
@@ -51,10 +63,14 @@ define( function( require ) {
         billiardsSceneNode.mutateRevealButton( { left: layoutBounds.left + 200 } );
       }
     } );
+
+    // Position the ABSwitch at the bottom center of the layout bounds.
     this.moveABSwitchToBottomCenter( abSwitch );
 
-    billiardsTableNode1.y = 20;
-    billiardsTableNode2.y = 20;
+    // Move the tables down a bit so they are centered vertically
+    var tableNodeOffsetY = 20;
+    billiardsTableNode1.y = tableNodeOffsetY;
+    billiardsTableNode2.y = tableNodeOffsetY;
   }
 
   proportionPlayground.register( 'BilliardsSceneNode', BilliardsSceneNode );
