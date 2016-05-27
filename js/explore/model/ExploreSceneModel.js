079ce791fa806fb0e0c5af173981b0810e9dd207
samreid <reids@colorado.edu>
2016-05-27 13:14:22 -0600

    Docs & refactor

--- js/explore/model/ExploreSceneModel.js
+++ js/explore/model/ExploreSceneModel.js
@@ -1,6 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
+ * The base class for an Explore/Predict scene.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -10,14 +11,21 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var PropertySet = require( 'AXON/PropertySet' );
+  var Property = require( 'AXON/Property' );
   var proportionPlayground = require( 'PROPORTION_PLAYGROUND/proportionPlayground' );
 
+  /**
+   *
+   * @param {boolean} predictMode - true for the Predict Screen which has a reveal button
+   * @param {Object} [otherProperties] - additional properties to be added to the PropertySet.
+   * @constructor
+   */
   function ExploreSceneModel( predictMode, otherProperties ) {
     this.predictMode = predictMode;
 
     PropertySet.call( this, _.extend( {
-      reveal: !predictMode, // {boolean} - true if the answer is being shown
-      showBoth: false // {boolean} - true if both representations should be shown
+      reveal: !predictMode, // {boolean} @public - true if the answer is being shown
+      showBoth: false // {boolean} @public - true if both representations should be shown
     }, otherProperties ) );
 
     // @public (read-only) These assignments provide improved highlighting and navigation in IntelliJ IDEA 
@@ -28,20 +36,21 @@ define( function( require ) {
   proportionPlayground.register( 'ExploreSceneModel', ExploreSceneModel );
 
   return inherit( PropertySet, ExploreSceneModel, {
+
+    /**
+     * Register properties that would trigger a revealed representation to be hidden, in the Predict screen.
+     *
+     * @param {Property[]} properties - the array of Property instances that cause the representation to be masked
+     * @protected
+     */
     registerChangeProperties: function( properties ) {
       assert && assert( this.predictMode, 'only register change properties for predict mode' );
       var exploreSceneModel = this;
 
       // In the predict screen, when one of the spinner is changed, hide the representations again.
-      var hide = function() {
+      Property.multilink( properties, function() {
         exploreSceneModel.reveal = false;
-      };
-      for ( var i = 0; i < properties.length; i++ ) {
-        properties[ i ].link( hide );
-      }
-    },
-    reset: function() {
-      PropertySet.prototype.reset.call( this );
+      } );
     }
   } );
 } );
