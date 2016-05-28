3511fdf2a1336ac7050cd171077cb941c5eeda74
samreid <reids@colorado.edu>
2016-05-28 09:45:05 -0600

    Added docs

--- js/explore/view/necklace/ControllableNecklaceNode.js
+++ js/explore/view/necklace/ControllableNecklaceNode.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * Combines a mutable NecklaceNode with its controls.
+ * Combines a mutable NecklaceNode with NumberPickers to change the amount of each type of bead.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -20,18 +20,41 @@ define( function( require ) {
   var RoundBeadNode = require( 'PROPORTION_PLAYGROUND/explore/view/necklace/RoundBeadNode' );
   var SquareBeadNode = require( 'PROPORTION_PLAYGROUND/explore/view/necklace/SquareBeadNode' );
 
+  // constants
+  var numberPickerOptions = { scale: 2 };
+
+  /**
+   *
+   * @param {NecklaceModel} necklaceModel - the model
+   * @param {Property.<boolean>} revealProperty - true if the necklace should be shown.
+   * @constructor
+   */
   function ControllableNecklaceNode( necklaceModel, revealProperty ) {
-    var numberPickerOptions = { scale: 2 };
+
+    // NumberPickers to choose the number of each type of bead
     var roundBeadNumberPicker = new NumberPicker( necklaceModel.roundBeadCountProperty, new Property( necklaceModel.beadCountRange ), numberPickerOptions );
     var squareBeadNumberPicker = new NumberPicker( necklaceModel.squareBeadCountProperty, new Property( necklaceModel.beadCountRange ), numberPickerOptions );
+
+    // The necklace itself
     var necklaceNode = new NecklaceNode( necklaceModel );
+
+    /**
+     * Adds an icon to a numberPicker
+     * @param {Node} icon - the icon to show above the NumberPicker
+     * @param {NumberPicker} numberPicker - the NumberPicker to be shown below the icon
+     * @returns {VBox}
+     */
     var toVBox = function( icon, numberPicker ) {
       return new VBox( {
         spacing: 10,
         children: [ icon, numberPicker ]
       } );
     };
+
+    // Only show the necklace when it is revealed
     revealProperty.linkAttribute( necklaceNode, 'visible' );
+
+    // Super call
     Node.call( this, {
       children: [
         necklaceNode,
