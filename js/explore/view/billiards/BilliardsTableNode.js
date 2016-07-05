23dd3f96568db4c551147aa93444ec5668409d44
andrea-phet <andrealin2018@gmail.com>
2016-07-05 16:17:27 -0600

    A start to making the tables draggable

--- js/explore/view/billiards/BilliardsTableNode.js
+++ js/explore/view/billiards/BilliardsTableNode.js
@@ -18,12 +18,19 @@ define( function( require ) {
   var Circle = require( 'SCENERY/nodes/Circle' );
   var ProportionPlaygroundConstants = require( 'PROPORTION_PLAYGROUND/ProportionPlaygroundConstants' );
   var Property = require( 'AXON/Property' );
+  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
+  var Util = require( 'DOT/Util' );
 
   // constants
   var scale = 18; // from model units to pixels
   var whiteStrokeOptions = { stroke: 'white' };
   var movingLineOptions = { stroke: 'white', lineWidth: 2 };
   var ballDiameter = 10;
+  var draggerOptions = {
+    fill: 'rgba(255,255,103,0.5)',
+    cursor: 'pointer',
+    pickable: true
+  };
 
   /**
    *
@@ -38,10 +45,41 @@ define( function( require ) {
     var linesNode = new Node();
     var currentLineNode = new Line( 0, 0, 0, 0, movingLineOptions );
 
+    // draw rectangles to help me see the coordinates, will remove
+    var centerRectangle = new Rectangle( center.x, center.y, 5, 5, { fill: 'red' } );
+    var zeroRectangle = new Rectangle( 0, 0, 5, 5, { fill: 'blue' } );
+    var startMouseRectangle = new Rectangle( 0, 0, 100, 5, { fill: 'black' } );
+    var mouseRectangle = new Rectangle( 0, 0, 5, 20, { fill: 'orange' } );
+
+    var helpLayer = new Node( {
+      children: [
+        centerRectangle,
+        zeroRectangle,
+        startMouseRectangle,
+        mouseRectangle
+      ]
+    } );
+
     // Model the edge outside of the green area (not as a stroke) since there is no way to do "outer" stroke
     var brownRectangle = new Rectangle( 0, 0, 0, 0, { fill: ProportionPlaygroundConstants.billiardsBrown } );
     var greenRectangle = new Rectangle( 0, 0, 0, 0, {
       fill: ProportionPlaygroundConstants.billiardsGreen
+    } );
+
+    // nodes used to drag the sides of the table
+    var leftDragger = new Rectangle( 0, 0, 0, 0, draggerOptions );
+    var rightDragger = new Rectangle( 0, 0, 0, 0, draggerOptions );
+    var topDragger = new Rectangle( 0, 0, 0, 0, draggerOptions );
+    var bottomDragger = new Rectangle( 0, 0, 0, 0, draggerOptions );
+
+    // separate layer for draggers
+    var draggersLayer = new Node( {
+      children: [
+        leftDragger,
+        rightDragger,
+        topDragger,
+        bottomDragger
+      ]
     } );
 
     // Layer containing the grid lines and ball lines.  When these were children of the greenRectangle, it caused #19
@@ -90,6 +128,52 @@ define( function( require ) {
       ballNode.center = position.times( scale ).plus( greenRectangle.translation );
     } );
 
+    var startX; // where the drag started
+    var startWidth; // the table width when drag started
+    var mouseX; // where the mouse is right now
+
+    // When the dragger is dragged, change the width
+    rightDragger.addInputListener( new SimpleDragHandler( {
+      allowTouchSnag: true,
+
+      // note where drag started
+      start: function( event ) {
+        startX = event.pointer.point.x;
+        startWidth = billiardsTableModel.width;
+        mouseX = startX;
+
+        helpLayer.removeChild( startMouseRectangle );
+        startMouseRectangle = new Rectangle( startX, event.pointer.point.y, 100, 5, { fill: 'black' } );
+        helpLayer.addChild( startMouseRectangle );
+
+        helpLayer.removeChild( mouseRectangle );
+        mouseRectangle = new Rectangle( mouseX, event.pointer.point.y, 5, 20, { fill: 'orange' } );
+        helpLayer.addChild( mouseRectangle );
+      },
+
+      drag: function( event ) {
+
+        // would want something like this, if mouse drag could match with view
+        // var matchedMouseX = undefined; // figure this out
+        // var newWidth = Util.roundSymmetric( ( matchedMouseX - center.x ) * 2 / scale );
+        // and then use newWidth instead of startWidth + widthChange below
+
+        mouseX = event.pointer.point.x;
+        var widthChange = Util.roundSymmetric( ( mouseX - startX ) * 2 / scale );
+
+        // change the width, making sure its within the acceptable range
+        billiardsTableModel.width = Util.clamp(
+          startWidth + widthChange,
+          billiardsTableModel.range.min,
+          billiardsTableModel.range.max
+        );
+
+        helpLayer.removeChild( mouseRectangle );
+        mouseRectangle = new Rectangle( mouseX, event.pointer.point.y, 5, 20, { fill: 'orange' } );
+        helpLayer.addChild( mouseRectangle );
+      }
+    } ) );
+
     // When the table is resized, redraw it.
     Property.multilink( [
       billiardsTableModel.lengthProperty,
@@ -104,6 +188,11 @@ define( function( require ) {
       var lineWidthAmount = brownEdgeLineWidth * 2;
       brownRectangle.setRect( 0, 0, scaledWidth + lineWidthAmount, scaledHeight + lineWidthAmount );
       greenRectangle.setRect( 0, 0, scaledWidth, scaledHeight );
+
+      leftDragger.setRect( 0, 0, lineWidthAmount / 2, scaledHeight );
+      rightDragger.setRect( 0, 0, lineWidthAmount / 2, scaledHeight );
+      topDragger.setRect( 0, 0, scaledWidth, lineWidthAmount / 2 );
+      bottomDragger.setRect( 0, 0, scaledWidth, lineWidthAmount / 2 );
 
       var createGridLines = function() {
         var gridLines = [];
@@ -123,9 +212,18 @@ define( function( require ) {
       // grid lines
       gridLinesNode.children = createGridLines();
 
+      // set the mouse and touch areas
+      // var hitBound = 2;
+      // rightDragger.mouseArea = rightDragger.bounds.dilated( hitBound );
+      // rightDragger.touchArea = rightDragger.mouseArea;
+
       // center the rectangles
       greenRectangle.center = center;
       brownRectangle.center = greenRectangle.center;
+      leftDragger.center = greenRectangle.center.plusXY( -( scaledWidth / 2 + lineWidthAmount / 4 ), 0 );
+      rightDragger.center = greenRectangle.center.plusXY( scaledWidth / 2 + lineWidthAmount / 4, 0 );
+      topDragger.center = greenRectangle.center.plusXY( 0, -( scaledHeight / 2 + lineWidthAmount / 4 ) );
+      bottomDragger.center = greenRectangle.center.plusXY( 0, scaledHeight / 2 + lineWidthAmount / 4 );
 
       // Position the lines layer
       lineLayer.translation = greenRectangle.translation;
@@ -140,11 +238,13 @@ define( function( require ) {
       children: [
         brownRectangle,
         greenRectangle,
+        draggersLayer,
         lineLayer,
         topLeftHoleNode,
         topRightHoleNode,
         bottomRightHoleNode,
-        ballNode
+        ballNode,
+        helpLayer
       ]
     } );
     this.mutate( options );
@@ -154,3 +254,4 @@ define( function( require ) {
 
   return inherit( Node, BilliardsTableNode );
 } );
+
