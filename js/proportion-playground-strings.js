610c4688b3c075e99e9bd61631d53330ee41a9b2
samreid <reids@colorado.edu>
2020-02-27 12:07:57 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/proportion-playground-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import proportionPlayground from './proportionPlayground.js';
+
+const proportionPlaygroundStrings = getStringModule( 'PROPORTION_PLAYGROUND' );
+
+proportionPlayground.register( 'proportionPlaygroundStrings', proportionPlaygroundStrings );
+
+export default proportionPlaygroundStrings;
