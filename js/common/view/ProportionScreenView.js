dae569830a7c1e6de5737aacb5efb9cff5a4846a
pixelzoom <cmalley@pixelzoom.com>
2020-10-12 15:13:33 -0600

    convert ScreenView subclasses to ES6 subclass, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/ProportionScreenView.js
+++ js/common/view/ProportionScreenView.js
@@ -8,7 +8,6 @@
  */
 
 import ScreenView from '../../../../joist/js/ScreenView.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import proportionPlayground from '../../proportionPlayground.js';
@@ -19,76 +18,70 @@ import NecklaceSceneNode from './necklace/NecklaceSceneNode.js';
 import PaintSceneNode from './paint/PaintSceneNode.js';
 import SceneSelectionControls from './SceneSelectionControls.js';
 
-/**
- * @constructor
- * @extends {ScreenView}
- *
- * @param {BarModel} model - the model
- */
-function ProportionScreenView( model ) {
-  const self = this;
+class ProportionScreenView extends ScreenView {
 
-  ScreenView.call( this );
+  /**
+   * @param {BarModel} model - the model
+   */
+  constructor( model ) {
+    super();
 
-  // Reset All button
-  this.addChild( new ResetAllButton( {
-    listener: function() {
-      model.reset();
-    },
-    centerX: this.layoutBounds.right - ProportionPlaygroundConstants.BUTTON_RIGHT_CENTER_OFFSET,
-    bottom: this.layoutBounds.maxY - 10
-  } ) );
+    // Reset All button
+    this.addChild( new ResetAllButton( {
+      listener: function() {
+        model.reset();
+      },
+      centerX: this.layoutBounds.right - ProportionPlaygroundConstants.BUTTON_RIGHT_CENTER_OFFSET,
+      bottom: this.layoutBounds.maxY - 10
+    } ) );
 
-  // @private {Array.<SceneNode>} - Store by index for lookup by radio button index
-  this.sceneNodes = [
-    new NecklaceSceneNode( model.necklaceScene, this.layoutBounds ),
-    new PaintSceneNode( model.paintScene, this.layoutBounds ),
-    new BilliardsSceneNode( model.billiardsScene, this.layoutBounds ),
-    new AppleSceneNode( model.appleScene, this.layoutBounds )
-  ];
+    // @private {Array.<SceneNode>} - Store by index for lookup by radio button index
+    this.sceneNodes = [
+      new NecklaceSceneNode( model.necklaceScene, this.layoutBounds ),
+      new PaintSceneNode( model.paintScene, this.layoutBounds ),
+      new BilliardsSceneNode( model.billiardsScene, this.layoutBounds ),
+      new AppleSceneNode( model.appleScene, this.layoutBounds )
+    ];
 
-  // Scene selection radio buttons
-  this.addChild( new SceneSelectionControls( model.sceneProperty, this.sceneNodes, {
-    centerX: this.layoutBounds.centerX,
-    top: 5
-  } ) );
+    // Scene selection radio buttons
+    this.addChild( new SceneSelectionControls( model.sceneProperty, this.sceneNodes, {
+      centerX: this.layoutBounds.centerX,
+      top: 5
+    } ) );
 
-  this.sceneNodes.forEach( function( scene ) {
-    scene.addShowBothSwitch();
-  } );
+    this.sceneNodes.forEach( function( scene ) {
+      scene.addShowBothSwitch();
+    } );
 
-  const sceneContainer = new Node( {
-    children: this.sceneNodes
-  } );
-  this.addChild( sceneContainer );
+    const sceneContainer = new Node( {
+      children: this.sceneNodes
+    } );
+    this.addChild( sceneContainer );
 
-  // When the scene radio button is selected, show the selected scene
-  model.sceneProperty.link( function( scene ) {
-    for ( let i = 0; i < self.sceneNodes.length; i++ ) {
-      const sceneNode = self.sceneNodes[ i ];
+    // When the scene radio button is selected, show the selected scene
+    model.sceneProperty.link( scene => {
+      for ( let i = 0; i < this.sceneNodes.length; i++ ) {
+        const sceneNode = this.sceneNodes[ i ];
 
-      const shouldBeVisible = sceneNode.scene === scene;
-      const isCurrentlyVisible = sceneNode.visible;
+        const shouldBeVisible = sceneNode.scene === scene;
+        const isCurrentlyVisible = sceneNode.visible;
 
-      // Interrupt
-      if ( !shouldBeVisible && isCurrentlyVisible ) {
-        sceneNode.interruptSubtreeInput();
+        // Interrupt
+        if ( !shouldBeVisible && isCurrentlyVisible ) {
+          sceneNode.interruptSubtreeInput();
+        }
+        sceneNode.visible = shouldBeVisible;
       }
-      sceneNode.visible = shouldBeVisible;
-    }
-  } );
-}
+    } );
+  }
 
-proportionPlayground.register( 'ProportionScreenView', ProportionScreenView );
-
-inherit( ScreenView, ProportionScreenView, {
   /**
    * Steps forward in time.
    * @public
    *
    * @param {number} dt - In seconds
    */
-  step: function( dt ) {
+  step( dt ) {
     // NOTE: uncapped dt in the view so that the transient animations finish when someone tabs out. See
     // https://github.com/phetsims/proportion-playground/issues/87 for more information.
 
@@ -97,6 +90,7 @@ inherit( ScreenView, ProportionScreenView, {
       this.sceneNodes[ i ].step( dt, visibleBounds );
     }
   }
-} );
+}
 
+proportionPlayground.register( 'ProportionScreenView', ProportionScreenView );
 export default ProportionScreenView;
