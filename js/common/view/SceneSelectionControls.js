6e23413c3ac7cd1b8debe3de9d16a242d622c0d3
Jonathan Olson <jonathan.olson@colorado.edu>
2020-11-10 13:23:18 -0700

    Drop extra Node layer in SceneSelectionControls

--- js/common/view/SceneSelectionControls.js
+++ js/common/view/SceneSelectionControls.js
@@ -6,14 +6,14 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import merge from '../../../../phet-core/js/merge.js';
 import AlignBox from '../../../../scenery/js/nodes/AlignBox.js';
 import AlignGroup from '../../../../scenery/js/nodes/AlignGroup.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
 import RectangularRadioButtonGroup from '../../../../sun/js/buttons/RectangularRadioButtonGroup.js';
 import proportionPlayground from '../../proportionPlayground.js';
 import ProportionPlaygroundColorProfile from './ProportionPlaygroundColorProfile.js';
 
-class SceneSelectionControls extends Node {
+class SceneSelectionControls extends RectangularRadioButtonGroup {
   /**
    * @param {Property.<Scene>} sceneProperty - Determines which scene is visible
    * @param {Array.<SceneNode>} sceneNodes - Each sceneNode has a scene and an icon
@@ -21,15 +21,13 @@ class SceneSelectionControls extends Node {
    * @param {Object} [options] - node options
    */
   constructor( sceneProperty, sceneNodes, tandem, options ) {
-    super();
-
     const group = new AlignGroup();
     const ratioItems = sceneNodes.map( sceneNode => ( {
       value: sceneNode.scene,
       node: new AlignBox( sceneNode.sceneIcon, { group: group } )
     } ) );
 
-    this.addChild( new RectangularRadioButtonGroup( sceneProperty, ratioItems, {
+    options = merge( {
       orientation: 'horizontal',
       buttonContentXMargin: 20,
       buttonContentYMargin: 12,
@@ -37,9 +35,9 @@ class SceneSelectionControls extends Node {
       selectedStroke: ProportionPlaygroundColorProfile.sceneSelectionBorderProperty,
       baseColor: ProportionPlaygroundColorProfile.sceneSelectionBackgroundProperty,
       tandem: tandem
-    } ) );
+    }, options );
 
-    this.mutate( options );
+    super( sceneProperty, ratioItems, options );
   }
 }
 
