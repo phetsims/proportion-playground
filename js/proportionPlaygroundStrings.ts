d0caec23fe13dd68c480470c58d13aa5dde73fc7
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:31 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/proportionPlaygroundStrings.ts
@@ -0,0 +1,33 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import proportionPlayground from './proportionPlayground.js';
+
+type StringsType = {
+  'proportion-playground': {
+    'title': string
+  },
+  'screen': {
+    'explore': string,
+    'predict': string
+  },
+  'costPerApple': string,
+  'totalCost': string,
+  'appleCost': string,
+  'apples': string,
+  'apple': string,
+  'length': string,
+  'pricePattern': string,
+  'pattern': string,
+  'width': string
+};
+
+const proportionPlaygroundStrings = getStringModule( 'PROPORTION_PLAYGROUND' ) as StringsType;
+
+proportionPlayground.register( 'proportionPlaygroundStrings', proportionPlaygroundStrings );
+
+export default proportionPlaygroundStrings;
