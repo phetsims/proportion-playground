30dae7ac78997e76b2c365989e912577a729c51a
phet-dev <phetadmin@gmail.com>
2022-06-24 04:05:43 -0600

    update copyright dates from daily grunt work

--- js/common/view/RevealButton.js
+++ js/common/view/RevealButton.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * In the Predict screen, the user can press the RevealButton to show the items.
