dae06f424fc1bad1596757934a11f47de8853175
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/proportionPlaygroundStrings.ts
+++ js/proportionPlaygroundStrings.ts
@@ -5,25 +5,38 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import proportionPlayground from './proportionPlayground.js';
 
 type StringsType = {
   'proportion-playground': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'explore': string;
+    'exploreProperty': TReadOnlyProperty<string>;
     'predict': string;
+    'predictProperty': TReadOnlyProperty<string>;
   };
   'costPerApple': string;
+  'costPerAppleProperty': TReadOnlyProperty<string>;
   'totalCost': string;
+  'totalCostProperty': TReadOnlyProperty<string>;
   'appleCost': string;
+  'appleCostProperty': TReadOnlyProperty<string>;
   'apples': string;
+  'applesProperty': TReadOnlyProperty<string>;
   'apple': string;
+  'appleProperty': TReadOnlyProperty<string>;
   'length': string;
+  'lengthProperty': TReadOnlyProperty<string>;
   'pricePattern': string;
+  'pricePatternProperty': TReadOnlyProperty<string>;
   'pattern': string;
+  'patternProperty': TReadOnlyProperty<string>;
   'width': string;
+  'widthProperty': TReadOnlyProperty<string>;
 };
 
 const proportionPlaygroundStrings = getStringModule( 'PROPORTION_PLAYGROUND' ) as StringsType;
