34acfa9b9b8082d59972c527e8a087f3cf6dfdfa
phet-dev <phetadmin@gmail.com>
2023-01-14 04:51:15 -0700

    update copyright dates from daily grunt work

--- js/common/view/paint/PaintSceneNode.js
+++ js/common/view/paint/PaintSceneNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, University of Colorado Boulder
+// Copyright 2016-2023, University of Colorado Boulder
 
 /**
  * Node that shows everything for a paint scene (including NumberPickers and paint splotches and gradient).
