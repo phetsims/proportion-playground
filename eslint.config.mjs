0792b61dcfb5ae5ece9120f8c0f432bb858b8b56
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:31 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for proportion-playground.
+ * ESLint configuration for proportion-playground.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
